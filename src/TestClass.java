004d4192751e236deaccce9e0a1d9e25ca9aef29
wiktuar <wiktuar@yandex.ru>
2022-10-02 12:59:32 +0300

    change in Java master

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,9 +2,14 @@ public class TestClass {
     public static void main(String[] args) {
         int[] numbers = {2, 5, 13, 7, 6, 4};
         int size = 6;
+        int tralala = 0
         int sum = 0;
         int avg = 0;
         int index = 0;
+
+        while (i < size){
+            System.out.println("Haha");
+        }
 
         while(index < size){
             sum+=numbers[index];
